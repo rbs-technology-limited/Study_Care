228259055a0e19369e1f964c6a613dfd3415f436
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-04 13:01:31 +0600

    update

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contact from "../../components/Contact/Contact";
+
+const page = async () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+
+      <Contact />
+    </div>
+  );
+};
+
+export default page;
