8cdd410f3d85d6f72b901c65476cef93d4c11fb9
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-06-06 12:30:54 +0600

    ax

---
+++ public/Asset/svg/ArrowRight.jsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+const ArrowRight = ({ color }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+
+
+    >
+        <path
+            stroke={color}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit={10}
+            strokeWidth={1.5}
+            d="m8.91 19.92 6.52-6.52c.77-.77.77-2.03 0-2.8L8.91 4.08"
+        />
+    </svg>
+)
+export default ArrowRight
--- src/components/Home/gallery/Gallery.tsx
+++ src/components/Home/gallery/Gallery.tsx
@@ -1,31 +1,102 @@
-import React from "react";
-import line from '../../../../public/Asset/Logo.png';
+import photo1 from '../../../../public/Asset/image/gallery/photos1.png';
+import photo2 from '../../../../public/Asset/image/gallery/photos2.png';
+import photo3 from '../../../../public/Asset/image/gallery/photos3.png';
+// import ArrowLeft from '../../../../public/Asset/svg/ArrowLeft';
+// import ArrowRight from '../../../../public/Asset/svg/ArrowRight';
+import Image from "next/image";
+import Slider from "react-slick";
 
-const video = [
+import line from '../../../../public/Asset/image/line.svg';
+const photos = [
     {
         id: 1,
-        title: 'Best Education System',
-        url: 'https://www.youtube.com/embed/tgbNymZ7vqY'
+        image: photo1
     },
     {
         id: 2,
-        title: 'Best Education System',
-        url: 'https://www.youtube.com/embed/tgbNymZ7vqY'
+        image: photo2
     },
     {
         id: 3,
-        title: 'Best Education System',
-        url: 'https://www.youtube.com/embed/tgbNymZ7vqY'
+        image: photo3
     },
     {
         id: 4,
-        title: 'Best Education System',
-        url: 'https://www.youtube.com/embed/tgbNymZ7vqY'
-    }
+        image: photo2
+    },
+    {
+        id: 5,
+        image: photo3
+    },
 ]
 export default function Gallery() {
+    // const slider = React.useRef(null);
+    const settings = {
+        className: "center",
+        infinite: true,
+        dots: false,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1.75,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
-        <section>
+        <section
+            className="position-relative z-10 overflow-hidden bg-white "
+        >
+            <div className="container mx-auto px-4 py-8">
+                <div className="flex flex-col justify-center items-center">
+                    <h2 className="text-black text-3xl font-bold text-center">
+                        Gallery
+                    </h2>
+                    <Image
+                        src={line}
+                        alt="Hero Image"
+                        width={152}
+                        height={13}
+                    />
+                </div>
+            </div>
+            <div className='gap-4'>
+
+            </div>
 
         </section>
     )
