cb05b86e5ef8bf1dbaf122f428e9b8f9299af1e1
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-06-06 16:36:15 +0600

    best teacher's

--- src/components/Home/freeNotes/FreeNotes.tsx
+++ src/components/Home/freeNotes/FreeNotes.tsx
@@ -9,7 +9,7 @@ export default function FreeNotes() {
         >
             <div className="inline-block w-[80%]  relative z-10  bg-black  rounded-2xl justify-center items-center overflow-hidden">
                 <div
-                    className="flex flex-col gap-4 relative justify-center items-center"
+                    className="flex flex-col gap-4 justify-center items-center py-6"
                 >
                     <h1
                         className="text-white text-[2.2rem] font-bold text-center py-2 mt-4"
@@ -18,9 +18,13 @@ export default function FreeNotes() {
                     <CButton variant="solid" color={teal} textUpperCased>
                         Download
                     </CButton>
-                    <div
-                        className="after:absolute after:top-0 after:left-0 after:w-[298px] after:h-[298] after:bg-[rgba(251, 193, 49, 0.5)] after:opacity-40 blur-[150px] "
-                    />
+
+                </div>
+                {/* left blur */}
+                <div className="absolute bottom-[-5rem] left-[-2rem] w-52 h-52 rounded-full bg-[#1BE1FF] opacity-40 blur-[100px] z-10">
+                </div>
+                {/* right blur */}
+                <div className="absolute top-[-5rem] right-[-2rem] w-52 h-52 rounded-full bg-[#FBC131] opacity-40 blur-[150px] z-10">
                 </div>
             </div>
         </div>
