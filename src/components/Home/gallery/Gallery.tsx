c01e5d2edfecf61bc9a4c30ce06c6fe08ee058fe
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-06-06 12:58:27 +0600

    re slick

--- src/components/Home/gallery/Gallery.tsx
+++ src/components/Home/gallery/Gallery.tsx
@@ -4,7 +4,6 @@ import photo3 from '../../../../public/Asset/image/gallery/photos3.png';
 // import ArrowLeft from '../../../../public/Asset/svg/ArrowLeft';
 // import ArrowRight from '../../../../public/Asset/svg/ArrowRight';
 import Image from "next/image";
-import Slider from "react-slick";
 
 import line from '../../../../public/Asset/image/line.svg';
 const photos = [
@@ -30,53 +29,6 @@ const photos = [
     },
 ]
 export default function Gallery() {
-    // const slider = React.useRef(null);
-    const settings = {
-        className: "center",
-        infinite: true,
-        dots: false,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-        speed: 500,
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1.75,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
     return (
         <section
             className="position-relative z-10 overflow-hidden bg-white "
