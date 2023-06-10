b6aa1517a7404357096b727b2b47434b64de86d0
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-10 14:31:14 +0600

    up

--- src/components/Home/gallery/Gallery.tsx
+++ src/components/Home/gallery/Gallery.tsx
@@ -1,11 +1,12 @@
-import photo1 from "../../../../public/Asset/image/gallery/photos1.png";
-import photo2 from "../../../../public/Asset/image/gallery/photos2.png";
-import photo3 from "../../../../public/Asset/image/gallery/photos3.png";
+import photo1 from "public/Asset/image/gallery/photos1.png";
+import photo2 from "public/Asset/image/gallery/photos2.png";
+import photo3 from "public/Asset/image/gallery/photos3.png";
 // import ArrowLeft from '../../../../public/Asset/svg/ArrowLeft';
 // import ArrowRight from '../../../../public/Asset/svg/ArrowRight';
 import Image from "next/image";
 
-import line from "../../../../public/Asset/image/line.svg";
+import line from "public/Asset/image/line.svg";
+
 const photos = [
   {
     id: 1,
@@ -28,16 +29,19 @@ const photos = [
     image: photo3,
   },
 ];
+
 export default function Gallery() {
   return (
-    <section className="position-relative z-10 overflow-hidden bg-white ">
-      <div className="container mx-auto px-4 py-8">
+    <section className="bg-white pt-16 py-20">
+      <div className="container mx-auto px-4 lg:px-8">
         <div className="flex flex-col justify-center items-center">
           <h2 className="text-black text-3xl font-bold text-center">Gallery</h2>
           <Image src={line} alt="line" width={152} height={13} />
         </div>
       </div>
-      <div className="gap-4"></div>
+      <div className="gap-4">
+
+      </div>
     </section>
   );
 }
---
+++ src/components/Home/gallery/GallrySlider/GallrySlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GallrySlider = () => {
+    return (
+        <div>
+                
+        </div>
+    );
+};
+
+export default GallrySlider;
