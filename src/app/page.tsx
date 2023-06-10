d2119d916f50e5cb6e04d493c10eba7f6a9843cb
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-10 15:17:06 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import ContactUs from "@/components/Home/ContactUs/ContactUs";
 import HeroSection from "@/components/Home/HeroSection/HeroSection";
 import MainFeature from "@/components/Home/MainFeature/MainFeature";
 import Faq from "@/components/Home/faq/Faq";
-import ContactUs from "@/components/Home/ContactUs/ContactUs";
 import About from "@/components/Home/About/About";
 import FreeNotes from "@/components/Home/freeNotes/FreeNotes";
 import Gallery from "@/components/Home/gallery/Gallery";
 import Workflow from "@/components/Home/workflow/Workflow";
 import PopularCourses from "@/components/Home/PopularCourses/PopularCourses";
 import ExploreCourses from "@/components/Home/ExploreCourses/ExploreCourses";
+import ContactUs from "@/components/Home/ContactUs/ContactUs";
 
 export default function Home() {
   return (
