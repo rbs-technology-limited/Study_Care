d81aa29bba3390dc6764c16aff309eab5aa4fbc0
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-11 15:32:12 +0600

    update bug

--- src/components/Home/reviews/Reviews.tsx
+++ src/components/Home/reviews/Reviews.tsx
@@ -2,59 +2,26 @@ import Image from "next/image";
 
 import line from "public/Asset/image/line.svg";
 import ReviewsSlider from "./ReviewsSlider";
-
+import { reviewsContent, reviewsData } from "@/Content";
 
 export default function Reviews() {
-    const reviewsData = {
-        sectiion_title: "Reviews",
-        reviews: [
-            {
-                id: 1,
-                name: 'Jenny Wilson',
-                designation: 'Students of BUET',
-                avatar: 'https://img.freepik.com/free-photo/bohemian-man-with-his-arms-crossed_1368-3542.jpg?w=740&t=st=1686411232~exp=1686411832~hmac=a40abf517257b633d44bd8207c615c34372d48c4cb1a9836336d4d39229711e1',
-                review_text: 'Thanks to WeCare transparent and efficient process, I was able to secure a spot at a prestigious Korean university.',
-
-            },
-            {
-                id: 2,
-                name: 'Tom Kerry',
-                designation: 'Student of RU',
-                avatar: 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1686411205~exp=1686411805~hmac=0e6d9002ef41b92b8343b26301df4f1deae7c44b4b80fff46e322ceeedd06245',
-                review_text: 'Thanks to WeCare transparent and efficient process, I was able to secure a spot at a prestigious Korean university.',
-
-            },
-            {
-                id: 3,
-                name: 'Leslie Winslate',
-                designation: 'Student of DU',
-                avatar: 'https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?w=996&t=st=1686411216~exp=1686411816~hmac=73d45d1695b7cf555b359c8c0298fe21996637afef0190bbd3596055f91175c9',
-                review_text: 'Thanks to WeCare transparent and efficient process, I was able to secure a spot at a prestigious Korean university.',
-
-            },
-
-
-        ]
-    }
-    return (
-        <section className="bg-white dark:bg-[#141414] relative overflow-hidden py-20">
-            <div className="container mx-auto px-4 lg:px-8 py-8">
-                <div className="flex flex-col justify-center items-center">
-                    <h2 className="text-black dark:text-white text-3xl font-bold text-center">
-                        {reviewsData.sectiion_title}
-                    </h2>
-                    <Image src={line} alt="line" width={100} height={13} />
-                </div>
-            </div>
-            <div className="gap-4">
-                <ReviewsSlider reviews={reviewsData.reviews} />
-            </div>
-            {/* left blur */}
-            <div className="absolute bottom-0 left-0 w-64 h-64 rounded-full bg-[#1BE1FF] opacity-40 blur-[200px]   z-10">
-            </div>
-            {/* right blur */}
-            <div className="absolute  top-0 right-0 w-64 h-64 rounded-full bg-[#FBC131] opacity-40 blur-[150px]  z-10">
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-white dark:bg-[#141414] relative overflow-hidden py-20">
+      <div className="container mx-auto px-4 lg:px-8 py-8">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-black dark:text-white text-3xl font-bold text-center">
+            {reviewsContent.title}
+          </h2>
+          <Image src={line} alt="line" width={100} height={13} />
+        </div>
+      </div>
+      <div className="gap-4">
+        <ReviewsSlider reviews={reviewsData} />
+      </div>
+      {/* left blur */}
+      <div className="absolute bottom-0 left-0 w-64 h-64 rounded-full bg-[#1BE1FF] opacity-40 blur-[200px]   z-10"></div>
+      {/* right blur */}
+      <div className="absolute  top-0 right-0 w-64 h-64 rounded-full bg-[#FBC131] opacity-40 blur-[150px]  z-10"></div>
+    </section>
+  );
 }
--- src/components/Home/reviews/ReviewsSlider.tsx
+++ src/components/Home/reviews/ReviewsSlider.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import {
-    BsFillArrowLeftCircleFill,
-    BsFillArrowRightCircleFill,
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
 } from "react-icons/bs";
 import "swiper/css/free-mode";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -19,151 +19,154 @@ import { Autoplay, FreeMode, Navigation, Pagination } from "swiper";
 // import HeroImage from "public/Asset/Hero_section_Logo.svg";
 import { CButton } from "@/Shared";
 // import { exploreCoursesData1 } from "@/Content";
-import { useSwiperSlide } from 'swiper/react';
+import { useSwiperSlide } from "swiper/react";
 
 interface reviewsData {
-    reviews?: Array<any>;
+  reviews?: Array<any>;
 }
 
 const ReviewsSlider = ({ reviews }: reviewsData) => {
-    const swiperSlide = useSwiperSlide();
-    const [init, setInit] = useState<any>(null);
-    React.useEffect(() => {
-        console.log(init);
-    }, [init]);
-    return (
-        <div className="w-full mt-4" data-aos="zoom-in">
-            <Swiper
-                slidesPerView={3}
-                spaceBetween={10}
-                loop={true}
+  //   const swiperSlide = useSwiperSlide();
+  const [init, setInit] = useState<any>(null);
 
-                centeredSlides={true}
-                freeMode={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={{
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                }}
-                allowTouchMove={true}
-                modules={[FreeMode, Pagination, Autoplay, Navigation]}
-                className="mySwiper"
-                onInit={(swiper) => {
-                    setInit(swiper);
-                }}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                    pauseOnMouseEnter: true,
-                }}
-                autoHeight={true}
-                breakpoints={{
-                    // when window width is >= 640px
-                    640: {
-                        slidesPerView: 1,
-                        spaceBetween: 10,
-                    },
-                    // when window width is >= 768px
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    // when window width is >= 1024px
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 10,
-                    },
+  //   React.useEffect(() => {
+  //     console.log(init);
+  //   }, [init]);
 
-                }}
-            >
-                {reviews?.map((review) => {
-                    return (
-                        <SwiperSlide
-                            key={review.id}
-                        >
-                            <div
-                                className={`flex flex-col justify-center items-center space-y-4 p-4 rounded-2xl 
+  return (
+    <div className="w-full mt-4" data-aos="zoom-in">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={true}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        allowTouchMove={true}
+        modules={[FreeMode, Pagination, Autoplay, Navigation]}
+        className="mySwiper"
+        onInit={(swiper) => {
+          setInit(swiper);
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        autoHeight={true}
+        breakpoints={{
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          // when window width is >= 1024px
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        }}
+      >
+        {reviews?.map((review) => {
+          return (
+            <SwiperSlide key={review.id}>
+              <div
+                className={`flex flex-col justify-center items-center space-y-4 p-4 rounded-2xl 
                                 shadow-lg
                                  bg-white
                                  dark:border-[#3F3F3F]  border-[#CDCDCD]
                                   py-16 px-4 
-                                ${init?.activeIndex === review.id ? 'dark:bg-[#0D0D0D] ' : 'dark:bg-transparent '}
-                                ${init?.activeIndex === review.id ? ' border-0' : ' border-2'}
+                                ${
+                                  init?.activeIndex === review.id
+                                    ? "dark:bg-[#0D0D0D] "
+                                    : "dark:bg-transparent "
+                                }
+                                ${
+                                  init?.activeIndex === review.id
+                                    ? " border-0"
+                                    : " border-2"
+                                }
                                 `}
-                            >
-
-                                <p
-                                    className={`text-xl font-normal text-center italic
+              >
+                <p
+                  className={`text-xl font-normal text-center italic
                                    
-                                    ${init?.activeIndex + 1 === review.id ? 'dark:text-[#868686] text-[#535353]' : ' text-[#CDCDCD] dark:text-[#868686]'}
+                                    ${
+                                      init?.activeIndex + 1 === review.id
+                                        ? "dark:text-[#868686] text-[#535353]"
+                                        : " text-[#CDCDCD] dark:text-[#868686]"
+                                    }
                                            `}
-                                >
-                                    {review?.review_text}
-                                </p>
-                                <div
-                                    className="flex  justify-center items-center gap-4"
-                                >
-                                    <div
-                                        className="relative mx-auto w-20 h-20 rounded-full overflow-hidden"
-                                    >
-                                        <Image
-                                            src={review?.avatar}
-                                            alt='avatar'
-                                            layout="fill"
-                                            objectFit="cover"
-                                            className="rounded-full"
-                                        />
-                                    </div>
+                >
+                  {review?.review_text}
+                </p>
+                <div className="flex  justify-center items-center gap-4">
+                  <div className="relative mx-auto w-20 h-20 rounded-full overflow-hidden">
+                    <Image
+                      src={review?.avatar}
+                      alt="avatar"
+                      fill
+                      objectFit="cover"
+                      className="rounded-full"
+                    />
+                  </div>
 
-                                    <div>
-                                        <h3
-
-                                            className={`
-                                            ${init?.activeIndex === review.id ? 'dark:text-[#00A6C0] text-[#0090A6]' : 'text-[#868686]'}
-                                        text-2xl font-bold
-                                        `}
-                                        >
-                                            {review?.name}
-                                        </h3>
-                                        <p
-                                            className={`
-                                        ${init?.activeIndex === review.id ? 'dark:text-white' : 'dark:text-[#727272] text-[#191919]'}
-                                    text-xl font-normal
-                                    `}
-                                        >
-                                            {review?.designation}
-                                        </p>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
-
-            {/* ??two button for next and prev */}
-            <div className="mt-10">
-                <div className="flex justify-center items-center space-x-4 mt-4">
-                    <CButton
-                        customClass="bg-transparent text-cyan-600 dark:text-cyan-500 "
-                        onClick={() => init?.slidePrev()}
+                  <div>
+                    <h3
+                      className={`${
+                        init?.activeIndex === review.id
+                          ? "dark:text-[#00A6C0] text-[#0090A6]"
+                          : "text-[#868686]"
+                      }text-2xl font-bold`}
                     >
-                        <BsFillArrowLeftCircleFill size={30} />
-                    </CButton>
-                    <CButton
-                        customClass="bg-transparent text-cyan-600 dark:text-cyan-500 "
-                        onClick={() => init?.slideNext()}
+                      {review?.name}
+                    </h3>
+                    <p
+                      className={`${
+                        init?.activeIndex === review.id
+                          ? "dark:text-white"
+                          : "dark:text-[#727272] text-[#191919]"
+                      }text-xl font-normal`}
                     >
-                        <BsFillArrowRightCircleFill size={30} />
-                    </CButton>
+                      {review?.designation}
+                    </p>
+                  </div>
                 </div>
-            </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      {/* ??two button for next and prev */}
+      <div className="mt-10">
+        <div className="flex justify-center items-center space-x-4 mt-4">
+          <CButton
+            customClass="bg-transparent text-cyan-600 dark:text-cyan-500 "
+            onClick={() => init?.slidePrev()}
+          >
+            <BsFillArrowLeftCircleFill size={30} />
+          </CButton>
+          <CButton
+            customClass="bg-transparent text-cyan-600 dark:text-cyan-500 "
+            onClick={() => init?.slideNext()}
+          >
+            <BsFillArrowRightCircleFill size={30} />
+          </CButton>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ReviewsSlider;
