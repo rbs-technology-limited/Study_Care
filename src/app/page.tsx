b7da2f776f1670bf17a7e4574cbb77a535d5d627
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-06-11 12:46:53 +0600

    review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import About from "@/components/Home/About/About";
+import ContactUs from "@/components/Home/ContactUs/ContactUs";
+import ExploreCourses from "@/components/Home/ExploreCourses/ExploreCourses";
 import HeroSection from "@/components/Home/HeroSection/HeroSection";
 import MainFeature from "@/components/Home/MainFeature/MainFeature";
+import PopularCourses from "@/components/Home/PopularCourses/PopularCourses";
 import Faq from "@/components/Home/faq/Faq";
-import About from "@/components/Home/About/About";
 import FreeNotes from "@/components/Home/freeNotes/FreeNotes";
 import Gallery from "@/components/Home/gallery/Gallery";
-import Workflow from "@/components/Home/workflow/Workflow";
-import PopularCourses from "@/components/Home/PopularCourses/PopularCourses";
 import Instructor from "@/components/Home/instructor/Instructor";
-import ExploreCourses from "@/components/Home/ExploreCourses/ExploreCourses";
-import ContactUs from "@/components/Home/ContactUs/ContactUs";
+import Reviews from "@/components/Home/reviews/Reviews";
+import Workflow from "@/components/Home/workflow/Workflow";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <ExploreCourses />
       <About />
       <Instructor />
+      <Reviews />
       <Faq />
       <Gallery />
       <ContactUs />
