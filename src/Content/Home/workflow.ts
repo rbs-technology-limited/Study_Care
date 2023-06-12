0ad166ccef9057e892514eb814baad407dca77e8
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-12 15:12:04 +0600

    workflow done

--- src/Content/Home/workflow.ts
+++ src/Content/Home/workflow.ts
@@ -1,53 +1,102 @@
 const workflowContent = {
-  content1: "Whatever you get with us",
-  content2:
-    "WeCare Education offers a comprehensive range of services, from admissions support",
+  // content1: "Whatever you get with us",
+  content1: "আমাদের সঙ্গে যা আপনি পাচ্ছেন",
+  content2: "আমরা ভর্তি সহায়তা সহ বিস্তারিত পরিসেবা সমূহ সরবরাহ করি।",
   content3:
-    "to visa assistance, to ensure a smooth and stress-free application",
-  content4: " process for our students.",
+    " স্কলারশিপ সহায়তা প্রদান করে আবেদনটি সহজ এবং তন্মুখ মুক্ত হওয়ার নিশ্চয়তা নিশ্চিত করতে।",
+  content4:
+    " আমাদের ছাত্রদের জন্য প্রক্রিয়াটির সাথে সম্পর্কিত নির্দিষ্ট বিষয়বস্তু সরবরাহ করা হয়।",
+  // content2:
+  //   "We offers a comprehensive range of services, from admissions support",
+  // content3:
+  //   "to visa assistance, to ensure a smooth and stress-free application",
+  // content4: " process for our students.",
 };
 
 export const workflowData = [
   {
     id: 1,
-    title: "Career Counselling",
+    title: "ক্যারিয়ার পরামর্শ",
   },
   {
     id: 2,
-    title: "University Selection",
+    title: "বিশ্ববিদ্যালয় নির্বাচন",
   },
   {
     id: 3,
-    title: "Application Process",
+    title: "আবেদন প্রক্রিয়া",
   },
   {
     id: 4,
-    title: "Class Schedule",
+    title: "ক্লাস সময়সূচি",
   },
   {
     id: 5,
-    title: "SSC Special Classes",
+    title: "এসএসসি বিশেষ ক্লাস",
   },
   {
     id: 6,
-    title: "Doubt Clearing Sessions",
+    title: "সন্দেহ পরিহার সেশন",
   },
   {
     id: 7,
-    title: "Mock Tests",
+    title: "মক টেস্ট",
   },
   {
     id: 8,
-    title: "Parent Teacher Meeting",
+    title: "অভিভাবক শিক্ষক সভা",
   },
   {
     id: 9,
-    title: "Progress Report",
+    title: "প্রগতি রিপোর্ট",
   },
   {
     id: 10,
-    title: "Feedback",
+    title: "মতামত",
   },
 ];
+
+//  [
+//   {
+//     id: 1,
+//     title: "Career Counselling",
+//   },
+//   {
+//     id: 2,
+//     title: "University Selection",
+//   },
+//   {
+//     id: 3,
+//     title: "Application Process",
+//   },
+//   {
+//     id: 4,
+//     title: "Class Schedule",
+//   },
+//   {
+//     id: 5,
+//     title: "SSC Special Classes",
+//   },
+//   {
+//     id: 6,
+//     title: "Doubt Clearing Sessions",
+//   },
+//   {
+//     id: 7,
+//     title: "Mock Tests",
+//   },
+//   {
+//     id: 8,
+//     title: "Parent Teacher Meeting",
+//   },
+//   {
+//     id: 9,
+//     title: "Progress Report",
+//   },
+//   {
+//     id: 10,
+//     title: "Feedback",
+//   },
+// ];
 
 export default workflowContent;
