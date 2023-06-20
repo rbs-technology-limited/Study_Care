fe87b7bf30b5f02e0e26dcc0a29c09e2c8d1eb56
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-20 21:24:21 +0600

    up

--- src/components/Course/CoursePageLeftUI/CoursePageLeftUI.tsx
+++ src/components/Course/CoursePageLeftUI/CoursePageLeftUI.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import CarouselMedia from "./CarouselMedia/CarouselMedia";
+import { coursePageData } from "@/Content";
 
 const CoursePageLeftUI = () => {
   return (
     <section>
       <CarouselMedia />
+      {/* //description  */}
+      <div className="mt-8">
+        <h1 className="text-2xl font-bold text-gray-800">
+          {coursePageData?.title}
+        </h1>
+        <p className="mt-4 text-gray-600">{coursePageData?.description}</p>
+      </div>
     </section>
   );
 };
