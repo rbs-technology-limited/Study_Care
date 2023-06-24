e30b39a990ac33176cbcd6d11878dedbc5c10998
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-06-24 18:14:51 +0600

    updated

--- src/components/Course/CoursePageLeftUI/AnyQuestionSection/AnyQuestionSection.tsx
+++ src/components/Course/CoursePageLeftUI/AnyQuestionSection/AnyQuestionSection.tsx
@@ -12,7 +12,7 @@ const AnyQuestionSection = () => {
         </h1>
 
         <section className="border-[.0925rem] p-3 my-5 rounded-lg w-[260px]">
-          <h1 className="text-lg font-semibold dark:text-white text-gray-800 flex justify-center items-center gap-2 text-[#4EBD7A]">
+          <h1 className="text-lg font-semibold flex justify-center items-center gap-2 text-[#4EBD7A]">
             {/* //call icon */}
             <BiPhoneCall />
             {anyQuestionData?.description}
