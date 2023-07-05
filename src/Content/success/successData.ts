0d40898a9260d378ce53d9c554d2f92ff7774fd8
thowhidulislam <thowhidulislam52@gmail.com>
2023-07-05 15:50:27 +0600

    [DONE] success register page ui

--- src/Content/success/successData.ts
+++ src/Content/success/successData.ts
@@ -3,7 +3,7 @@ export const registerSuccessStaticData = {
   message1:
     "We have successfully created your new account. But before you start you will have to activate it. We have sent an activation mail to the email you provided during registration. It should arrive in a couple of minutes",
   message2:
-    "IF the email has not arrived during 30s you can still click the button below to resend it. We guarantee it will come this time! ",
+    "If the email has not arrived during 30s you can still click the button below to resend it. We guarantee it will come this time! ",
   link: "Re-send the verification email",
   button: "Close",
 };
--- src/components/SuccessRegister/SuccessRegister.tsx
+++ src/components/SuccessRegister/SuccessRegister.tsx
@@ -8,8 +8,8 @@ import SuccessSvg2 from "../svgComponents/SuccesSvg2";
 const SuccessRegister = () => {
   return (
     <section className="container-lg relative x-auto overflow-hidden">
-      <div className="text-center h-screen flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold">
+      <div className="dark:text-white text-center h-screen flex flex-col justify-center items-center">
+        <h1 className="text-3xl dark:text-white font-bold">
           {registerSuccessStaticData.title}
         </h1>
         <p className="my-6 w-1/2">{registerSuccessStaticData.message1}</p>
@@ -21,13 +21,18 @@ const SuccessRegister = () => {
         />
       </div>
       <div className="flex">
-        <div className="none lg:absolute left-[-150px] bottom-[-10px]">
+        <div className="none lg:absolute left-[-130px] xl:left-[-100px] bottom-[-10px]">
           <SuccessSvg1 />
         </div>
-        <div className="none lg:absolute right-[-105px] bottom-[-15px]">
+        <div className="none lg:absolute lg:right-[-135px] xl:right-[-105px] bottom-[-15px]">
           <SuccessSvg2 />
         </div>
       </div>
+
+      {/* left blur */}
+      <div className="hidden dark:block dark:absolute bottom-0 left-0 w-64 h-64 rounded-full bg-[#1BE1FF] opacity-40 blur-[100px] z-10"></div>
+      {/* right blur */}
+      <div className="hidden dark:block dark:absolute top-0 right-0 w-64 h-64 rounded-full bg-custom-orange opacity-40 blur-[100px] z-10"></div>
     </section>
   );
 };
