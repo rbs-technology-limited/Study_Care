ffc999af9ed3f3c412e8a011c1dd9386d01e84f3
Md. Thowhidul Islam Molla <thowhidulislam52@gmail.com>
2023-07-09 21:54:14 +0600

    [DONE] teacher profile ui

--- src/components/TeaherProfile/TeacherProfile.tsx
+++ src/components/TeaherProfile/TeacherProfile.tsx
@@ -56,89 +56,71 @@ const TeacherProfile = () => {
   });
 
   return (
-    <section className="container-lg mx-auto px-4 md:px-16 py-8">
+    <section className="container mx-auto px-4 md:px-16 py-8">
       {/*  search part  */}
       <div className="flex justify-end">
         <div className="w-full md:w-2/4 lg:w-1/3 flex flex-col md:flex-row gap-2 md:items-center">
-          <CInput
-            mb="0"
-            //   onChange={handleInputChange}
-            placeholder="Search"
-          />
-          <div className="w-2/5">
-            <CButton
-              variant="solid"
-              color="teal"
-              btnTitle="Search"
-              // onClick={handleSearch}
-            />
-          </div>
+          <CInput mb="0" placeholder="Search" />
+          <CButton variant="solid" color="teal" btnTitle="Search" />
         </div>
       </div>
       {/* main part */}
-      <div className="w-full flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-2 my-5 text-gray-600 text-sm w-full lg:w-9/12">
-          {/* left side */}
-          <div className="col-span-1 md:col-span-2">
-            {/* image with details */}
-            <div className="flex flex-col md:flex-row md:items-center gap-4">
-              <div>
-                <Image
-                  src={image}
-                  alt="teacher image"
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <div>
-                <p className="dark:text-white">
-                  <span className="font-bold">নামঃ</span> {name}
-                </p>
-                <p className="dark:text-white">
-                  <span className="font-bold">পদবীঃ</span> {designation}
-                </p>
-                <p className="dark:text-white">
-                  <span className="font-bold">মোবাইলঃ</span> {phone}
-                </p>
-                <p className="dark:text-white">
-                  <span className="font-bold">ইমেইলঃ</span> {email}
-                </p>
-                <p className="dark:text-white">
-                  <span className="font-bold">ঠিকানাঃ</span> {address}
-                </p>
-              </div>
-            </div>
-
-            {/* right side for small devices */}
-            <div className="col-span-1 sm:block md:hidden">
-              <p className="font-bold p-3 text-gray-800 dark:text-white border-[0.093rem] rounded-sm border-button-teal">
-                ফেরদৌস45
-              </p>
-            </div>
-
-            {/* education part */}
-            <div className="my-7">
-              <h1 className="text-xl text-gray-800 dark:text-white font-bold my-2">
-                শিক্ষাগত যোগ্যতা
-              </h1>
-              {educationContents}
-            </div>
-
-            {/* experiences part */}
+      <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-2 my-5 text-gray-600 text-sm">
+        {/* left side */}
+        <div className="col-span-1 md:col-span-2">
+          {/* image with details */}
+          <div className="flex flex-col md:flex-row md:items-center gap-4">
             <div>
-              <h1 className="text-xl text-gray-800 dark:text-white font-bold my-2">
-                পেশাগত অভিজ্ঞতা
-              </h1>
-              {experiencesContents}
+              <Image src={image} alt="teacher image" width={200} height={200} />
+            </div>
+            <div>
+              <p className="dark:text-white">
+                <span className="font-bold">নামঃ</span> {name}
+              </p>
+              <p className="dark:text-white">
+                <span className="font-bold">পদবীঃ</span> {designation}
+              </p>
+              <p className="dark:text-white">
+                <span className="font-bold">মোবাইলঃ</span> {phone}
+              </p>
+              <p className="dark:text-white">
+                <span className="font-bold">ইমেইলঃ</span> {email}
+              </p>
+              <p className="dark:text-white">
+                <span className="font-bold">ঠিকানাঃ</span> {address}
+              </p>
             </div>
           </div>
 
-          {/* right side */}
-          <div className="hidden md:block col-span-1">
-            <p className="font-bold p-3 text-gray-800 dark:text-white border-[0.093rem] rounded-sm border-button-teal">
+          {/* right side for small devices */}
+          <div className="col-span-1 sm:block md:hidden mt-7">
+            <p className="font-bold p-3 text-gray-800 dark:text-white border-[0.0937rem] rounded-md border-button-teal">
               ফেরদৌস45
             </p>
           </div>
+
+          {/* education part */}
+          <div className="my-7">
+            <h1 className="text-xl text-gray-800 dark:text-white font-bold my-2">
+              শিক্ষাগত যোগ্যতা
+            </h1>
+            {educationContents}
+          </div>
+
+          {/* experiences part */}
+          <div>
+            <h1 className="text-xl text-gray-800 dark:text-white font-bold my-2">
+              পেশাগত অভিজ্ঞতা
+            </h1>
+            {experiencesContents}
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="hidden md:block col-span-1">
+          <p className="font-bold p-3 text-gray-800 dark:text-white border-[0.0937rem] rounded-md border-button-teal">
+            ফেরদৌস45
+          </p>
         </div>
       </div>
     </section>
