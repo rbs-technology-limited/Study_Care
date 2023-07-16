6a61b8f76c3d9be547a071c57f9a997317ca1859
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-16 16:55:59 +0600

    color used in student dashboard profile progress

--- src/components/StudentDashboard/StudentProfile/CourseProgressInfos/CourseProgressInfos.tsx
+++ src/components/StudentDashboard/StudentProfile/CourseProgressInfos/CourseProgressInfos.tsx
@@ -27,10 +27,10 @@ const CourseProgressInfos = ({ data }: ICourseProgress) => {
     return (
       <React.Fragment key={item.id}>
         <div
-          className={`${conditions} ${firstElementLeftBorderRadius} border-gray-200 p-2 text-sm`}
+          className={`${conditions} ${firstElementLeftBorderRadius} border-gray-200 px-2 py-4 text-sm`}
         >
           <h1 className="text-lg font-bold text-gray-600">{item.title}</h1>
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center my-2">
             <h1 className="text-gray-600 flex items-center">
               <span className="text-xl font-bold text-button-teal mr-[0.0625rem]">
                 {item?.total_modules} টি
@@ -39,8 +39,10 @@ const CourseProgressInfos = ({ data }: ICourseProgress) => {
                 (বাকি আছে {item.incomplete_module} টি)
               </span>
             </h1>
-            <HiOutlineArrowUp />
-            <span className="bg-custom-teal rounded-lg">{item?.progress}</span>
+            <div className="flex items-center gap-1 rounded-full bg-[#DFF4D7] text-[#51933E] p-2">
+              <HiOutlineArrowUp />
+              <span className="rounded-lg">{item?.progress}%</span>
+            </div>
           </div>
         </div>
       </React.Fragment>
@@ -48,8 +50,8 @@ const CourseProgressInfos = ({ data }: ICourseProgress) => {
   });
 
   return (
-    <section className="mt-24">
-      <h1 className="text-xl font-bold text-gray-600 my-2">
+    <section className="mt-24 dark:text-white">
+      <h1 className="text-xl font-bold text-gray-600 my-3">
         কোর্স অগ্রগতি - Batch-2
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3">
