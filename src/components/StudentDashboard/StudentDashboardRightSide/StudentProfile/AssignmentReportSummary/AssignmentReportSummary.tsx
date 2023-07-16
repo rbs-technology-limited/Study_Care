ea2ea1a2f0eaafea323507f599902da8145b6878
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-07-16 19:26:53 +0600

    up

---
+++ src/components/StudentDashboard/StudentDashboardRightSide/StudentProfile/AssignmentReportSummary/AssignmentReportSummary.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AssignmentReportSummary = () => {
+    return (
+        <div>
+            AssignmentReportSummary
+        </div>
+    );
+};
+
+export default AssignmentReportSummary;
--- src/components/StudentDashboard/StudentDashboardRightSide/StudentProfile/StudentProfile.tsx
+++ src/components/StudentDashboard/StudentDashboardRightSide/StudentProfile/StudentProfile.tsx
@@ -2,13 +2,15 @@ import { profileData } from "@/Content";
 import CourseProgressInfos from "./CourseProgressInfos/CourseProgressInfos";
 import UserInfo from "./UserInfo/UserInfo";
 import OverAllCourseProgress from "./OverAllCourseProgress/OverAllCourseProgress";
+import AssignmentReportSummary from "./AssignmentReportSummary/AssignmentReportSummary";
 
 const StudentProfile = () => {
   return (
     <div>
       <UserInfo data={profileData} />
       <CourseProgressInfos data={profileData.courseProgressData} />
-      <OverAllCourseProgress/>
+      <OverAllCourseProgress />
+      <AssignmentReportSummary />
     </div>
   );
 };
