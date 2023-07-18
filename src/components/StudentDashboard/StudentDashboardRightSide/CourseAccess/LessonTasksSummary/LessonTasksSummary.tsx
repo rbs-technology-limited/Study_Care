9748f6156c4d59ba04ef19495516b57c5942e414
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-18 19:50:43 +0600

    course access - icon used

--- src/components/StudentDashboard/StudentDashboardRightSide/CourseAccess/LessonTasksSummary/LessonTasksSummary.tsx
+++ src/components/StudentDashboard/StudentDashboardRightSide/CourseAccess/LessonTasksSummary/LessonTasksSummary.tsx
@@ -1,17 +1,47 @@
 "use client";
+import { teal } from "@/Constant/Custom-Color";
 import { courseAccessData, courseAccessStaticData } from "@/Content";
+import { AiOutlineStop } from "react-icons/ai";
+import { FiPlayCircle } from "react-icons/fi";
+import { LuFiles } from "react-icons/lu";
+import { SiGoogleclassroom } from "react-icons/si";
 import CopyClassroomLink from "./CopyClassroomLink/CopyClassroomLink";
 
+const getStatusIcon = (status: string) => {
+  switch (status) {
+    case "কুইজ আছে":
+      return <LuFiles color={teal} />;
+    case "কুইজ নেই":
+      return <AiOutlineStop color="red" />;
+    default:
+      return <AiOutlineStop color="red" />;
+  }
+};
+
 const LessonTasksSummary = () => {
+  const quizStatusIcon = getStatusIcon(courseAccessData.quiz_status);
+  const assignmentStatusIcon = getStatusIcon(
+    courseAccessData.assignment_status
+  );
   return (
     <section>
-      <div className="p-3 bg-gray-50 text-gray-600 dark:text-white border rounded flex flex-col md:flex-row justify-between items-center gap-2">
-        <p className="p-2">{courseAccessData.quiz_status}</p>
-        <p className="p-2">
+      <div className="p-3 bg-gray-50 text-sm text-gray-600 dark:text-white border rounded flex flex-col md:flex-row justify-between items-center gap-2">
+        <p className="flex gap-1 items-center">
+          {quizStatusIcon}
+          <span>{courseAccessData.quiz_status}</span>
+        </p>
+        <p className="flex gap-1 items-center">
+          <SiGoogleclassroom color={teal} />
           <CopyClassroomLink data={courseAccessData.classroom_link} />
         </p>
-        <p className="p-2">{courseAccessData.assignment_status}</p>
-        <p className="p-2">{courseAccessStaticData.record_video_button}</p>
+        <p className="flex gap-1 items-center">
+          {assignmentStatusIcon}
+          <span>{courseAccessData.assignment_status}</span>
+        </p>
+        <p className="flex gap-1 items-center cursor-pointer">
+          <FiPlayCircle color={teal} />
+          <span>{courseAccessStaticData.record_video_button}</span>
+        </p>
       </div>
     </section>
   );
