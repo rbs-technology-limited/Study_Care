a3aeee1b70deedeedeb9cc6ed6505888290122b2
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-18 20:06:47 +0600

    course access - made responsive

--- src/components/StudentDashboard/StudentDashboardRightSide/CourseAccess/CourseAccess.tsx
+++ src/components/StudentDashboard/StudentDashboardRightSide/CourseAccess/CourseAccess.tsx
@@ -3,13 +3,15 @@ import LessonTasksSummary from "./LessonTasksSummary/LessonTasksSummary";
 const CourseAccess = () => {
   return (
     <section className="container mx-auto px-8">
-      <div className="flex flex-col md:flex-row gap-3">
+      <div className="flex flex-col-reverse lg:flex-row gap-3">
         {/* description */}
-        <div className="w-3/5">
+        <div className="w-full lg:w-3/5">
           <LessonTasksSummary />
         </div>
         {/* lesson part */}
-        <div className="w-2/5"></div>
+        <div className="w-full lg:w-2/5">
+          <h1>Lesson list</h1>
+        </div>
       </div>
     </section>
   );
