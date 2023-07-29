2a3d3911483ca94218d136a0451abce39852023d
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-07-29 19:48:17 +0600

    a

--- src/Global/(cockies)/getCoockies.ts
+++ src/Global/(cockies)/getCoockies.ts
@@ -4,7 +4,6 @@ import { cookies } from "next/headers";
 
 export const getTokenCookies = async (name = "authToken") => {
   const { value }: any = cookies()?.get(name) || {};
-  // console.log(value);
   const token = value ? JSON.parse(value) : null;
   return token || null;
 };
